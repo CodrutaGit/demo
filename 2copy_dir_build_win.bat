5ebb2ce90cc5f403c2e525a0ab92bf0b72c5dade
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-09-09 14:59:03 +0300

    Add files via upload

--- 2copy_dir_build_win.bat
+++ 2copy_dir_build_win.bat
@@ -1,6 +1,6 @@
-net use \\172.27.112.113\sw_transforms\public\ITM\Kit\IPM_4.9.0\win\cycle12\ /u:saguaroprint.ro\cmorariu New42day /p:yes
+net use \\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9 /u:saguaroprint.ro\cmorariu New42day /p:yes
 
-set source="\\172.27.112.113\sw_transforms\public\ITM\Kit\IPM_4.9.0\win\cycle12\"
+set source="\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9"
 set target="\\172.27.105.152\Users\Administrator\Transforms\Builds"
 
 FOR /F "delims=" %%I IN ('DIR %source%\*.* /A:-D /O:-D /B') DO COPY %source%\"%%I" %target% & echo %%I & GOTO :END
