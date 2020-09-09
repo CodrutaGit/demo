816d57a63bd6a2aa89247fd77b31025b8de840a0
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-09-09 12:30:41 +0300

    Add files via upload

--- 4create_folder_structure_win.bat
+++ 4create_folder_structure_win.bat
@@ -17,6 +17,6 @@ echo mkdir E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_PDF\demo_TestFile
 mkdir E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_TIFF\demo_MF\
 echo mkdir E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_TIFF\demo_MF\
 
-c:\Windows\System32\xcopy.exe /T  E:\OutputTransformPDF2AFP_APPE\demo_structura_foldere\demo_output\demo_TestFiles\* E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_AFP\demo_TestFiles\ /s /i
-c:\Windows\System32\xcopy.exe /T  E:\OutputTransformPDF2AFP_APPE\demo_structura_foldere\demo_output\demo_TestFiles\* E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_PDF\demo_TestFiles\ /s /i
-c:\Windows\System32\xcopy.exe /T  E:\OutputTransformPDF2AFP_APPE\demo_structura_foldere\demo_output\demo_TestFiles\* E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_TIFF\demo_MF\ /s /i
+c:\Windows\System32\xcopy.exe /T /e  E:\OutputTransformPDF2AFP_APPE\demo_structura_foldere\demo_output\demo_TestFiles\* E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_AFP\demo_TestFiles\ /s /i
+c:\Windows\System32\xcopy.exe /T /e E:\OutputTransformPDF2AFP_APPE\demo_structura_foldere\demo_output\demo_TestFiles\* E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_PDF\demo_TestFiles\ /s /i
+c:\Windows\System32\xcopy.exe /T /e E:\OutputTransformPDF2AFP_APPE\demo_structura_foldere\demo_output\demo_TestFiles\* E:\OutputTransformPDF2AFP_APPE\demo_win\demo_output_TIFF\demo_MF\ /s /i
