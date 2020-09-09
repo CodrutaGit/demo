a1c2bb80a6558d8d1d000ef2d1f0f247ee30de30
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-09-09 18:36:35 +0300

    Add files via upload

--- 2copy_dir_build_win.bat
+++ 2copy_dir_build_win.bat
@@ -1,6 +1,3 @@
 net use \\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9 /u:saguaroprint.ro\cmorariu New42day /p:yes
 
-set source="\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9"
-set target="\\172.27.105.152\Users\Administrator\Transforms\Builds"
-
 c:\Windows\System32\xcopy.exe /T  \\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9 \\172.27.105.152\Users\Administrator\Transforms\Builds /s /i
