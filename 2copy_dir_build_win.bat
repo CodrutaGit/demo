8c46f7e021fcfa3f2bcc585815672f88b8a1b498
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-09-24 14:03:05 +0300

    Add files via upload

--- 2copy_dir_build_win.bat
+++ 2copy_dir_build_win.bat
@@ -1,5 +1,6 @@
 net use \\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9 /u:saguaroprint.ro\cmorariu New42day /p:yes
 
+
 copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setupitm.exe" "\\172.27.105.152\Builds"
 copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setupafp2pdf.exe" "\\172.27.105.152\Builds"
 copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setuppcl2afp.exe" "\\172.27.105.152\Builds"
--- 5install_all_TF_ipm_win.bat
+++ 5install_all_TF_ipm_win.bat
@@ -1,2 +1,3 @@
 cd "D:\Builds\"
+
 call D:\Builds\install_TF_IPM.cmd
