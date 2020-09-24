b79aa765abcea05bb5407fb34fd4dc42adb06f49
CodrutaGit <64213593+CodrutaGit@users.noreply.github.com>
2020-09-24 15:02:41 +0300

    Add files via upload

--- 2copy_dir_build_win.bat
+++ 2copy_dir_build_win.bat
@@ -1,9 +1,4 @@
 net use \\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9 /u:saguaroprint.ro\cmorariu New42day /p:yes
+ 
 
-
-copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setupitm.exe" "\\172.27.105.152\Builds"
-copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setupafp2pdf.exe" "\\172.27.105.152\Builds"
-copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setuppcl2afp.exe" "\\172.27.105.152\Builds"
-copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setupps2afp.exe" "\\172.27.105.152\Builds"
-copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setupsap2afp.exe" "\\172.27.105.152\Builds"
-copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\setuppdf2afp.exe" "\\172.27.105.152\Builds"
+copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\*.*" "d:\Builds\"
